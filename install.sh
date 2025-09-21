63f022ae3c8708e8933fdbf524f8970c4b2db120
Jamie Brain <82174996+jamiebrn@users.noreply.github.com>
2025-09-21 20:08:06 +0100

    upload install

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -e ~/lldb_breakpoint_hook.py ]; then
+    read -r -p "Already installed. Install anyway? y/N: " response
+    if [[ "$response" != "y" && "$response" != "Y" && "$response" != "yes" && "$response" != "Yes" ]]; then
+        echo "Aborting installation"
+        exit 1
+    fi
+fi
+
+if [ -e ~/.lldbinit ]; then
+    printf "\e[0;31mWARNING:\e[0m .lldbinit already exists. Overwrite? y/N:"
+    read -r response
+    if [[ "$response" != "y" && "$response" != "Y" && "$response" != "yes" && "$response" != "Yes" ]]; then
+        echo "Aborting installation"
+        exit 1
+    fi
+fi
+
+cp lldb_breakpoint_hook.py ~/lldb_breakpoint_hook.py
+cp .lldbinit ~/.lldbinit
+
+echo "Installed successfully"
